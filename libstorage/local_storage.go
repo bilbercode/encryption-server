76105442702ee7913ffe5b695dce6fea286840e1
owain.llewellyn <bilbercode@gmail.com>
2018-01-30 22:51:55 +0000

    feat: Local storage functionality
    Added local storage functionality to the service
    
    GitHub issue #3

--- libstorage/local_storage.go
+++ libstorage/local_storage.go
@@ -1,6 +1,10 @@
 package libstorage
 
-import "sync"
+import (
+	"sync"
+	"errors"
+	"fmt"
+)
 
 type LocalStorage struct {
 	items map[string][]byte
@@ -8,18 +12,37 @@ type LocalStorage struct {
 }
 
 func (s *LocalStorage) Store(data []byte, key string) error {
-	panic("not implemented")
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	s.items[key] = data
+	return nil
 }
 
 func (s *LocalStorage) Retrieve(key string) ([]byte, error) {
-	panic("not implemented")
+	s.lock.RLock()
+	defer s.lock.RUnlock()
+	value, ok := s.items[key]
+	if !ok {
+		return nil, errors.New(fmt.Sprintf("key `%s` does not exist", key))
+	}
+
+	return value, nil
 }
 
 
 func (s *LocalStorage) Has(key string) (bool, error) {
-	panic("not implemented")
+	s.lock.RLock()
+	defer s.lock.RUnlock()
+	if _, ok := s.items[key]; ok {
+		return true, nil
+	}
+	return false, nil
 }
 
+// provides a new LocalStorage
 func NewLocalStorage() Storage {
-	panic("not implemented")
+	return &LocalStorage{
+		items: map[string][]byte{},
+		lock: sync.RWMutex{},
+	}
 }
