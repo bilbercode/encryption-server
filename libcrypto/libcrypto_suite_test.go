e4dbfefcf98d7669fdbd6b193bdb1b928a26c4ed
owain.llewellyn <bilbercode@gmail.com>
2018-01-30 22:23:13 +0000

    chore: Updated CI build instructions
    Added the testing requirements to the CI server

--- libcrypto/libcrypto_suite_test.go
+++ libcrypto/libcrypto_suite_test.go
@@ -5,9 +5,11 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/onsi/ginkgo/reporters"
 )
 
 func TestLibcrypto(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "Libcrypto Suite")
+	junitReporter := reporters.NewJUnitReporter("junit.xml")
+	RunSpecsWithDefaultAndCustomReporters(t, "Libcrypto Suite", []Reporter{junitReporter})
 }
